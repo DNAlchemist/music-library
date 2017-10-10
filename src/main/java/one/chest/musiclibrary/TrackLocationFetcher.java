e20d19586b23a444d010a251a00a53e969d19194
Ruslan Mikhalev <RMikhalev@alfabank.ru>
2017-10-10 16:50:47 +0300

    Update for Java 9 compatibility: package javax.xml.bind.annotation.adapters is not visible

--- src/main/java/one/chest/musiclibrary/TrackLocationFetcher.java
+++ src/main/java/one/chest/musiclibrary/TrackLocationFetcher.java
@@ -25,7 +25,6 @@ package one.chest.musiclibrary;
 
 import one.chest.musiclibrary.exception.MusicLibraryException;
 
-import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -33,7 +32,6 @@ import java.security.NoSuchAlgorithmException;
 public class TrackLocationFetcher {
 
     private final MessageDigest signer;
-    HexBinaryAdapter hexBinaryAdapter = new HexBinaryAdapter();
 
     TrackLocationFetcher() {
         try {
@@ -50,9 +48,21 @@ public class TrackLocationFetcher {
 
     private String md5(String source) {
         try {
-            return hexBinaryAdapter.marshal(signer.digest(source.getBytes("utf8"))).toLowerCase();
+            return toHexString(signer.digest(source.getBytes("utf8"))).toLowerCase();
         } catch (UnsupportedEncodingException e) {
             throw new AssertionError("UTF8 encoding is unsupported!");
         }
+    }
+
+    private static final char[] hexCode = "0123456789ABCDEF".toCharArray();
+
+    private String toHexString(byte[] data) {
+        if (data == null) return null;
+        StringBuilder r = new StringBuilder(data.length * 2);
+        for (byte b : data) {
+            r.append(hexCode[(b >> 4) & 0xF]);
+            r.append(hexCode[(b & 0xF)]);
+        }
+        return r.toString();
     }
 }
